eac7b26e09059596770fbbef63ab6054bf886292
fkfkfk999 <fkfkfk9@daum.net>
2019-06-20 13:56:37 +0900

    express로 처음 서버 만들어 테스트

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const express = require("express");
+const app = express();
+const PORT = 3333;
+
+function listeningHandler() {
+  console.log(`http://127.0.0.1:${PORT} 서버 시작!`);
+}
+
+app.listen(PORT, listeningHandler);
