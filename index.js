3269c7e35b7eb84f75cc6b73c1d5668ab5a3b550
fkfkfk999 <fkfkfk9@daum.net>
2019-06-20 15:34:53 +0900

    바벨, nodemon 설치 완료

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
 const app = express();
 const PORT = 3333;
 
-function listeningHandler() {
+// function listeningHandler() {
+//   console.log(`http://127.0.0.1:${PORT} 서버 시작!`);
+// }
+// function homeHandler(req, res) {
+//   console.log("Home Start!!");
+//   res.send("Home Start!!");
+// }
+// function profileHandler(req, res) {
+//   console.log("Profile Start!!");
+//   res.send("Profile Start!!");
+// }
+const listeningHandler = () =>
   console.log(`http://127.0.0.1:${PORT} 서버 시작!`);
-}
+
+const homeHandler = (req, res) => {
+  console.log("Home Start!!");
+  res.send("Home Start!!");
+};
+const profileHandler = (req, res) => {
+  console.log("Profile Start!!");
+  res.send("Profile Start!!");
+};
+
+app.get("/", homeHandler);
+app.get("/profile", profileHandler);
 
 app.listen(PORT, listeningHandler);
