f466dab9cd16680293eb98b9da9ff346f9d9950d
fkfkfk999 <fkfkfk9@daum.net>
2019-06-24 17:42:24 +0900

    Pug에 변수 넣는 방법 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -10,12 +11,15 @@ import routes from "./routes";
 
 const app = express();
 
+// 설치한 미들웨어 설정
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(morgan("dev"));
+// 사용자가 만든 미들웨어 설정
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,9 +1,14 @@
 //user
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("video/search");
+export const home = (req, res) => res.render("home", { pageTitle: "Root" });
+export const search = (req, res) =>
+  res.render("video/search", { pageTitle: "Search" });
 
 //video
-export const video_upload = (req, res) => res.render("video/video_upload");
-export const video_detail = (req, res) => res.render("video/video_detail");
-export const edit_video = (req, res) => res.render("video/edit_video");
-export const delete_video = (req, res) => res.render("video/delete_video");
+export const video_upload = (req, res) =>
+  res.render("video/video_upload", { pageTitle: "Upload" });
+export const video_detail = (req, res) =>
+  res.render("video/video_detail", { pageTitle: "Video Detail" });
+export const edit_video = (req, res) =>
+  res.render("video/edit_video", { pageTitle: "Edit Video" });
+export const delete_video = (req, res) =>
+  res.render("video/delete_video", { pageTitle: "Delete Video" });
