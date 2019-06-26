fbe8358fdea551e00a206d7e2824017951e9fb10
fkfkfk999 <fkfkfk9@daum.net>
2019-06-26 13:17:34 +0900

    form으로 get방식 데이터 전송하기

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,14 +1,26 @@
 //user
 export const home = (req, res) => res.render("home", { pageTitle: "Root" });
-export const search = (req, res) =>
-  res.render("video/search", { pageTitle: "Search" });
+export const search = (req, res) => {
+  // Es5
+  //const searchingBy = req.query.searchWrd;
+  // Es6
+  const {
+    query: { searchWrd: searchingBy }
+  } = req;
+  console.log(req.query);
+  res.render("video/search", { pageTitle: "Search", searchingBy });
+};
 
 //video
-export const video_upload = (req, res) =>
+export const video_upload = (req, res) => {
   res.render("video/video_upload", { pageTitle: "Upload" });
-export const video_detail = (req, res) =>
+};
+export const video_detail = (req, res) => {
   res.render("video/video_detail", { pageTitle: "Video Detail" });
-export const edit_video = (req, res) =>
+};
+export const edit_video = (req, res) => {
   res.render("video/edit_video", { pageTitle: "Edit Video" });
-export const delete_video = (req, res) =>
+};
+export const delete_video = (req, res) => {
   res.render("video/delete_video", { pageTitle: "Delete Video" });
+};
