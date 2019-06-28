9ebf4de5c9520a6ac41ee181a48da40da41d1970
fkfkfk999 <fkfkfk999@gmail.com>
2019-06-28 23:59:21 +0900

    Login, Join 처리 등

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,7 +11,7 @@ const userRouter = express.Router();
 
 userRouter.get(routes.edit_profile, edit_profile);
 userRouter.get(routes.change_password, change_password);
-userRouter.get(routes.users_detail, user_detail);
+userRouter.get(routes.users_detail(), user_detail);
 
 //기존에는 변수에 export를 해줬는데 그럴 경우 변수만 익스포트 되고
 //아래처럼 할 경우 전체가 다 익스포트 된다.
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "../routes";
 import {
   video_home,
-  video_upload,
+  video_getUpload,
+  video_postUpload,
   video_detail,
   edit_video,
   delete_video
@@ -10,9 +11,10 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.upload, video_upload);
+videoRouter.get(routes.upload, video_getUpload);
+videoRouter.post(routes.upload, video_postUpload);
 videoRouter.get(routes.edit_video, edit_video);
 videoRouter.get(routes.delete_video, delete_video);
-videoRouter.get(routes.videos_detail, video_detail);
+videoRouter.get(routes.videos_detail(), video_detail);
 
 export default videoRouter;
