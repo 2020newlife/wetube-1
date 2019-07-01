e7934fa890e43564c46c69e2c1b99d1f6e9bb973
fkfkfk999 <fkfkfk9@daum.net>
2019-07-01 17:14:01 +0900

    mongoDB연동

--- init.js
+++ init.js
@@ -1,6 +1,9 @@
+import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () =>
   console.log(`http://127.0.0.1:${PORT} 경로로 서버 시작!`)
